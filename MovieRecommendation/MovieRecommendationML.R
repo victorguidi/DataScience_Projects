23600091bd1697fac5cb7f53981157dbc14974d0
Victor Guidi <77369161+victorguidi@users.noreply.github.com>
2021-09-12 13:16:33 -0300

    Final Result

--- MovieRecommendation/MovieRecommendationML.R
+++ MovieRecommendation/MovieRecommendationML.R
@@ -164,6 +164,15 @@ table_views[1:6, ] #The first 6 movies
 View(table_views)
 tail(table_views)
 
+#Bar plot with the ratings (excluding the ones with 0)
+table_of_Ratings_df <- as.data.frame(table_of_Ratings)
+table_of_Ratings_df <- table_of_Ratings_df[-1,]
+ggplot(table_of_Ratings_df, aes(x= Freq, y = rating_values)) +
+  geom_bar(stat="identity", fill = 'cyan') +
+  geom_text(aes(label = Freq ), vjust=-0.3, size=3.5) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Total")
+
 #Plotting a bar plot with ggplot to see the number of views for the top movies than for the not so popular
 ggplot(table_views[1:6, ], aes(x = title, y = views)) +
   geom_bar(stat="identity", fill ='darkgreen') +
@@ -178,5 +187,149 @@ ggplot(tail(table_views), aes(x = title, y = views)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ggtitle("Total Views of the bottom Films")
 
-
 #Heatmap
+image(ratingMatrix[1:20, 1:25], axes = FALSE, main = "Heatmap of the first 25 rows and 25 columns")
+
+#FIFTH PART - DATA PREPARATION
+# 3 steps:
+#   Selecting useful data
+#   Normalizing data - Organizing it and turning it into a structured form
+#   Binarizing the data - is the process of dividing data into two groups and assigning one out. of two values
+#                          to all the members of the same group
+
+
+#Selecting the data  to at least 50 people that rated a movie and to movies that have at least 50 views
+movie_ratings <- ratingMatrix[rowCounts(ratingMatrix) > 50,
+                              colCounts(ratingMatrix) > 50]
+movie_ratings # 420 x 447 rating matrix of class ‘realRatingMatrix’ with 38341 ratings.
+
+#Creating a matrix with relevant user using quantile regression
+
+#Quantile regression models the relationship between a set of predictor (independent) variables and specific percentiles (or "quantiles") 
+#of a target (dependent) variable, most often the median - Definition by IBM
+
+minimum_movies <- quantile(rowCounts(movie_ratings), 0.98)
+minimum_users <- quantile(colCounts(movie_ratings), 0.98)
+image(movie_ratings[rowCounts(movie_ratings) > minimum_movies,
+                    colCounts(movie_ratings) > minimum_users],
+      main = "Heatmap of the top users and movies")
+
+#Looking at the distribution of the average rating per user using a Histogram
+average_ratings <- rowMeans(movie_ratings)
+qplot(average_ratings, fill=I("steelblue"), col=I("red")) +
+  ggtitle("Distribution of the average rating per user")
+
+#Data Normalization
+#Normalization is a data preparation procedure to standardize the numerical values in a column to a common scale value.
+#This is done in such a way that there is no distortion in the range of values.
+#Normalization transforms the average value of our ratings column to 0.
+
+normalized_ratings <- normalize(movie_ratings)
+sum(rowMeans(normalized_ratings) > 0.00001)
+
+image(normalized_ratings[rowCounts(normalized_ratings) > minimum_movies,
+                         colCounts(normalized_ratings) > minimum_users],
+      main = "Normalized Ratings of the Top User")
+
+#Data Binarization
+#Binarizing the data means that we have two discrete values 1 and 0, which will allow our recommendation systems to work more efficiently.
+#If the rating is above 3 is equal to 1 if not is equal to 0
+
+binary_minimum_movies <- quantile(rowCounts(movie_ratings), 0.95)
+binary_minimum_users <- quantile(colCounts(movie_ratings), 0.95)
+
+good_rated_films <- binarize(movie_ratings, minRating = 3)
+image(good_rated_films[rowCounts(movie_ratings) > binary_minimum_movies,
+      colCounts(movie_ratings) > binary_minimum_users],
+main = "Heatmap of the top users and movies")
+
+#Build the recommendation System
+
+#First we have to divide the data into training and test (0.8 training, 0.2 test)
+sampled_data <- sample(x = c(TRUE, FALSE),
+                      size = nrow(movie_ratings),
+                      replace = TRUE,
+                      prob = c(0.8, 0.2))
+training_data <- movie_ratings[sampled_data, ]
+testing_data <- movie_ratings[!sampled_data]
+
+training_data #332 x 447 rating matrix of class ‘realRatingMatrix’ with 31363 ratings.
+testing_data #88 x 447 rating matrix of class ‘realRatingMatrix’ with 6978 ratings.
+
+#Buildin the system
+recommendation_system <- recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+recommendation_model$IBCF_realRatingMatrix$parameters
+
+#Training the model
+recommen_model <- Recommender(data = training_data,
+                              method = "IBCF",
+                              parameter = list(k = 30))
+recommen_model
+class(recommen_model)
+
+#Creating a matrix that stores the results of the model
+model_info <- getModel(recommen_model)
+class(model_info$sim)
+dim(model_info$sim)
+top_items <- 20
+image(model_info$sim[1: top_items, 1: top_items],
+      main = "Heatmap of the first rows and columns")
+
+#Now we calculate the sum of rows and columns that have similarity above 0 (That is have been rated above 3)
+sum_rows <- rowSums(model_info$sim > 0)
+table(sum_rows)
+sum_cols <- colSums(model_info$sim > 0)
+qplot(sum_cols, fill=I("steelblue"), col=I("red"))+ ggtitle("Distribution of the column count")
+
+#Final Part
+#Now we determines the top 10 by creating a variable and use the predict() function that will identify similar items and will rank them
+#each rating is used as a weight. Each weight is multiplied with related similarities.
+
+top_recommendations <- 10 # the number of items to recommend to each user
+predicted_recommendations <- predict(object = recommen_model,
+                                     newdata = testing_data,
+                                     n = top_recommendations)
+predicted_recommendations
+
+
+#Now we select an user from the testing data and try to recommend the top 10 to make sure it works
+user1 <- predicted_recommendations@items[[1]] # recommendation for the first user
+movies_user1 <- predicted_recommendations@itemLabels[user1]
+movies_user2 <- movies_user1
+for (index in 1:10){
+  movies_user2[index] <- as.character(subset(movie_data,
+                                             movie_data$movieId == movies_user1[index])$title)
+}
+movies_user2
+
+#Checking how many recommendations we have for each user in the testing data, and adding it to a matrix
+recommendation_matrix <- sapply(predicted_recommendations@items,
+                                function(x){ as.integer(colnames(movie_ratings)[x]) }) # matrix with the recommendations for each user
+View(recommendation_matrix[,1:4])
+
+#Finding the distribution of number of items for IBCF
+number_of_items <- factor(table(recommendation_matrix))
+chart_title <- "Distribution of the Number of Items for IBCF"
+qplot(number_of_items, fill=I("steelblue"), col=I("black")) + ggtitle(chart_title)
+
+#Top 10 movies more recommended:
+number_of_items_sorted <- sort(number_of_items, decreasing = TRUE)
+number_of_items_top <- head(number_of_items_sorted, n = 10)
+table_top_10 <- data.frame(as.integer(names(number_of_items_top)),
+                        number_of_items_top)
+for(i in 1:10) {
+  table_top_10[i,1] <- as.character(subset(movie_data,
+                                        movie_data$movieId == table_top_10[i,1])$title)
+}
+
+colnames(table_top_10) <- c("Movie_Title", "No_of_Items")
+View(table_top_10)
+
+#Visualizing the top 10 movies more recommended:
+ggplot(table_top_10, aes(x= Movie_Title, y = No_of_Items)) +
+  geom_bar(stat="identity", fill = 'darkgreen') +
+  geom_text(aes(label = No_of_Items ), vjust=-0.3, size=3.5) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Top 10 movies more recommended")
+
+#Finish
